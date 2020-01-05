9ddb0d0bd3e88fa7a3e63516009c947466176623
Victory Asokomeh <victoryasokomeh@gmail.com>
2020-01-05 23:29:55 +0100

    implemented logout use

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,43 @@
-const asyncHandler = require('../middleware');
+const jwt = require('jsonwebtoken');
+const { asyncHandler } = require('../middleware');
+const ErrorRespose = require('../utils/errorResponse');
+const { User } = require('../models');
 
-// exports.protect = asyncHandler(async (req, res, next) => {
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
 
-// })
+  if (!token) {
+    return next(new ErrorRespose(`Not authorized to access this route`, 401));
+  }
+
+  try {
+    const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decodeToken.id);
+
+    next();
+  } catch (error) {
+    return next(new ErrorRespose(`Not authorized to access this route`, 401));
+  }
+});
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorRespose(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,2 +1,3 @@
 exports.asyncHandler = require('./asyncHandler');
 exports.errorHandler = require('./errorHandler');
+exports.auth = require('./auth');
