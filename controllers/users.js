80b6c9229727f854273a47d9558786344154c137
Victory Asokomeh <victoryasokomeh@gmail.com>
2020-01-07 23:47:36 +0100

    implemented unfollow

--- controllers/users.js
+++ controllers/users.js
@@ -2,37 +2,64 @@ const ErrorResponse = require('../utils/errorResponse');
 const { asyncHandler } = require('../middleware');
 const { User } = require('../models');
 
+// Search DB with provided id
+const searchById = (model, id) => {
+  const result = model.findById(id);
+  return result;
+};
+
+// Get all users
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({ success: true, data: users });
+});
+
+// Get single user
 exports.getUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.userId).populate({
-    path: 'following',
-    select: 'name'
-  });
+  const user = await searchById(User, req.params.userId);
 
   res.status(200).json({ success: true, data: user });
 });
 
+// Follow a user
 exports.follow = asyncHandler(async (req, res, next) => {
-  const userToBeFollowed = await User.findById(req.params.requestedUser);
+  const userToFollow = await searchById(User, req.params.requesteduser);
 
-  if (!userToBeFollowed) {
-    return next(
-      new ErrorResponse(
-        `User with ID ${req.params.requestedUser} not found`,
-        404
-      )
-    );
+  if (!userToFollow) {
+    return next(new ErrorResponse(`User not found`, 404));
   }
 
-  const user = await User.findById(req.user.id);
+  const self = await searchById(User, req.user.id);
 
-  if (user.id === req.params.requestedUser) {
+  if (self.id === req.params.requesteduser) {
     return next(new ErrorResponse(`Can't follow self`, 400));
-  } else if (user.following.includes(req.params.requestedUser)) {
+  } else if (self.following.includes(req.params.requesteduser)) {
     return next(new ErrorResponse(`Already following user`, 400));
   }
 
-  user.following.push(userToBeFollowed);
-  await user.save();
+  self.following.push(userToFollow);
+  await self.save();
 
-  res.status(200).json({ success: true, data: user });
+  res.status(200).json({ success: true, data: self });
 });
+
+exports.unfollow = asyncHandler(async (req, res, next) => {
+  const userToUnfollow = await searchById(User, req.params.requesteduser);
+
+  if (!userToUnfollow) {
+    return next(new ErrorResponse(`User not found`, 404));
+  }
+
+  const self = await searchById(User, req.user.id);
+
+  if (self.id === req.params.requesteduser) {
+    return next(new ErrorResponse(`Can't unfollow self`, 400));
+  } else if (self.following.includes(req.params.requesteduser)) {
+    return next(new ErrorResponse(`Currently not following user`, 400));
+  }
+
+  self.following.pull(userToUnfollow);
+  res.status(200).json({ success: true, data: self });
+});
+
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { follow, getUser } = require('../controllers/users');
+const { follow, getUser, getUsers, unfollow } = require('../controllers/users');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
+router.get('/', getUsers);
 router.get('/:userId', getUser);
-router.put('/follow/:requestedUser', protect, follow);
+router.put('/follow/:requesteduser', protect, follow);
+router.put('/unfollow/:requesteduser', protect, unfollow);
 
 module.exports = router;
