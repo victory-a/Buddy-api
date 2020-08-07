9bb7e0ab0488050b9c6a808fa4a31516731d779e
dev-soks <victoryasokomeh@josla.com.ng>
2020-08-07 22:34:03 +0100

    switched to cloud db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL, {
+  const conn = await mongoose.connect(process.env.MONGO_URI_CLOUD, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
