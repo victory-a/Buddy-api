6d33e5ceff8bdcc61a471e01be0d4f49b8312523
dev-soks <victoryasokomeh@josla.com.ng>
2020-08-31 20:07:23 +0100

    worked on db error handling

--- config/db.js
+++ config/db.js
@@ -2,7 +2,18 @@ const mongoose = require('mongoose');
 
 // mongoose.set('bufferCommands', false);
 module.exports = async function() {
+  const db = mongoose.connection;
   try {
+    db.on('connected', function() {
+      console.log(`DB connected successfully on ${db.host}`.green.underline);
+    });
+    db.on('disconnected', function() {
+      console.log(`Mongoose disconnected`.red.underline);
+    });
+    db.on('error', function(err) {
+      console.log(`Error occured ${err}`.red.underline);
+    });
+
     return await mongoose.connect(process.env.MONGO_URI_CLOUD, {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -13,15 +24,3 @@ module.exports = async function() {
     console.log(`Error occured ${error}`.red.underline);
   }
 };
-
-const db = mongoose.connection;
-
-db.on('connected', function() {
-  console.log(`DB connected successfully on ${db.host}`.green.underline);
-});
-db.on('disconnected', function() {
-  console.log(`Mongoose disconnected`.red.underline);
-});
-db.on('error', function(err) {
-  console.log(`Error occured ${err}`.red.underline);
-});
